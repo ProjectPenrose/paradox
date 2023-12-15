55dbdf2a71ea6b15c1afcebcdb55552b1083ed50
Santiago <rincorpes@gmail.com>
2023-12-15 09:51:11 -0500

    Add router class with options and init method

--- src/core/Router.js
+++ src/core/Router.js
@@ -1,4 +1,13 @@
+/**
+ * Represents a router that handles routing and navigation in a web application.
+ */
 export default class Router {
+  /**
+   * Creates a new instance of the Router class.
+   * @param {Object} options - The options for configuring the router.
+   * @param {Array} options.routes - An array of route objects.
+   * @param {string} options.baseUrl - The base URL of the application.
+   */
   constructor(options = { routes: [], baseUrl: "" }) {
     const { routes = [], baseUrl = "" } = options;
 
@@ -16,6 +25,11 @@ export default class Router {
     this.memo = {};
   }
 
+  /**
+   * Initializes the router and navigates to the appropriate route.
+   * @returns {Promise<string>} A promise that resolves to the current path.
+   * @throws {Error} If the route is not found or an error occurs during navigation.
+   */
   async init() {
     let { path } = this;
     const { routes, queryString } = this;
