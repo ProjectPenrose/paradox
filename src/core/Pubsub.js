5368ce362e244da55792237cca5a1ad513521d9b
Santiago <rincorpes@gmail.com>
2023-12-15 11:58:48 -0500

    Add subscribe and publish methods to PubSub class

--- src/core/Pubsub.js
+++ src/core/Pubsub.js
@@ -1,8 +1,17 @@
+/**
+ * PubSub class for implementing publish-subscribe pattern.
+ */
 class PubSub {
   constructor() {
     this.events = {};
   }
 
+  /**
+   * Subscribe to an event.
+   * @param {string} event - The event name to subscribe to.
+   * @param {function} callback - The callback function to be executed when the event is published.
+   * @returns {number} - The number of callbacks subscribed to the event.
+   */
   subscribe(event, callback) {
     let self = this;
     if (!self.events.hasOwnProperty(event)) {
@@ -12,6 +21,12 @@ class PubSub {
     return self.events[event].push(callback);
   }
 
+  /**
+   * Publish an event.
+   * @param {string} event - The event name to publish.
+   * @param {object} [data={}] - The data to be passed to the event subscribers.
+   * @returns {array} - An array of return values from the event subscribers.
+   */
   publish(event, data = {}) {
     let self = this;
     if (!self.events.hasOwnProperty(event)) {
