045567666b71a8190dc658f6aec78a72a7ee4992
Santiago <rincorpes@gmail.com>
2023-12-15 09:50:03 -0500

    Refactor buildElement function to include detailed options

--- src/core/buildElement.js
+++ src/core/buildElement.js
@@ -41,7 +41,20 @@ function getStyleKey(key) {
 // WeakMap to store event listeners for each element
 const eventListeners = new WeakMap();
 
-// Main function that creates a DOM element with provided specifications
+/**
+ * Builds and returns an HTML element based on the provided tag and options.
+ *
+ * @param {string} tag - The HTML tag for the element.
+ * @param {Object} [options] - The options for configuring the element.
+ * @param {string} [options.id=""] - The ID of the element.
+ * @param {string} [options.classList=""] - The space-separated list of classes for the element.
+ * @param {Array} [options.children=[]] - The array of child elements.
+ * @param {Object} [options.attributes={}] - The key-value pairs of attributes for the element.
+ * @param {Object} [options.events={}] - The key-value pairs of event listeners for the element.
+ * @param {string} [options.text=""] - The text content of the element.
+ * @param {Object} [options.style={}] - The key-value pairs of inline styles for the element.
+ * @returns {HTMLElement} - The constructed HTML element.
+ */
 export default function buildElement(tag, options = { id: "", classList: "", children: [], attributes: {}, events: {}, text: "", style: {} }) {
   // Return empty string if tag is not provided
   if (!tag) return "";
