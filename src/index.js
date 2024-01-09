dab2917e703bbf84eb860b3a556b294ec69369c4
alexsc6955 <rincorpes@gmail.com>
2024-01-09 16:32:45 -0500

    Fix import case in pubsub module

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import buildElement from "./core/buildElement";
 import Router from "./core/Router";
-import pubsub from "./core/pubsub";
+import pubsub from "./core/Pubsub";
 
 const Paradox = {
   buildElement,
