55293b65145d61050a342e13ae2f2df26c283561
alexsc6955 <rincorpes@gmail.com>
2024-01-10 20:06:19 -0500

    Add tests for buildElement options

--- build/index.test.js
+++ build/index.test.js
@@ -20,6 +20,75 @@ describe('Paradox', () => {
             let element = buildElement("div");
             expect(element instanceof HTMLElement).toBe(true);
         });
+        describe('options', () => {
+            const options = {
+                id: "test",
+                classList: "test test2",
+                attributes: { "data-test": "test" },
+                text: "test",
+                style: { color: "red" },
+                children: Array(),
+                events: {}
+            };
+            let element = buildElement("div", options);
+            it("should set the ID if provided", () => {
+                expect(element.id).toBe(options.id);
+            });
+            it("should add classes if provided", () => {
+                const classList = options.classList.split(" ");
+                for (let className of classList) {
+                    expect(element.classList.contains(className)).toBe(true);
+                }
+            });
+            it("should set attributes if provided", () => {
+                for (let [key, value] of Object.entries(options.attributes)) {
+                    expect(element.getAttribute(key)).toBe(value);
+                }
+            });
+            it("should set text content if provided", () => {
+                expect(element.textContent).toBe(options.text);
+            });
+            it("should set inline styles if provided", () => {
+                for (let [key, value] of Object.entries(options.style)) {
+                    expect(element.style[key]).toBe(value);
+                }
+            });
+            describe("children", () => {
+                const children = [
+                    { tag: "span" },
+                    { tag: "span" },
+                    {
+                        tag: "span", options: {
+                            children: [
+                                { tag: "span" },
+                                { tag: "span" }
+                            ]
+                        }
+                    }
+                ];
+                options.children = children;
+                element = buildElement("div", options);
+                it("should append children if provided", () => {
+                    expect(element.children.length).toBe(children.length);
+                });
+                it("should append nested children if provided", () => {
+                    expect(element.children[2].children.length).toBe(children[2].options.children.length);
+                });
+            });
+            describe("events", () => {
+                const events = {
+                    click: () => { },
+                    mouseover: () => { }
+                };
+                options.events = events;
+                element = buildElement("div", options);
+                it("should add event listeners if provided", () => {
+                    for (let [key, value] of Object.entries(events)) {
+                        expect(element.listeners.has(key)).toBe(true);
+                    }
+                });
+            });
+        });
     });
     it('should have Router property', () => {
         expect(index_1.default.hasOwnProperty("Router")).toBe(true);
