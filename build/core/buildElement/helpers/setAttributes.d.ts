8c33af1521abe5951a7c339bcf679cac1bc3039d
alexsc6955 <rincorpes@gmail.com>
2024-01-17 14:37:14 -0500

    Add setAttributes function documentation

--- build/core/buildElement/helpers/setAttributes.d.ts
+++ build/core/buildElement/helpers/setAttributes.d.ts
@@ -1,3 +1,9 @@
+/**
+ * Sets the attributes of an HTML element.
+ * If the attribute is a boolean attribute, it will only be set if the value is truthy.
+ * @param element - The HTML element to set the attributes for.
+ * @param attributes - An object containing the attribute key-value pairs.
+ */
 declare function setAttributes(element: HTMLElement, attributes: {
     [key: string]: string;
 }): void;
