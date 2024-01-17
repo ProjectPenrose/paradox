55bb677444c1c1286ee3509e309f0e95670ef982
alexsc6955 <rincorpes@gmail.com>
2024-01-17 14:38:18 -0500

    Update handleEvents function signature

--- build/core/buildElement/helpers/handleEvents.js
+++ build/core/buildElement/helpers/handleEvents.js
@@ -2,6 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 // WeakMap to store event listeners for each element
 const eventListeners = new WeakMap();
+/**
+ * Attaches event listeners to an HTML element.
+ *
+ * @param element - The HTML element to attach the event listeners to.
+ * @param events - An object containing event names as keys and event listeners as values.
+ */
 function handleEvents(element, events) {
     // Retrieve or create the event listeners Map for this particular element
     let elementEvents = eventListeners.get(element);
--- src/core/buildElement/helpers/handleEvents.ts
+++ src/core/buildElement/helpers/handleEvents.ts
@@ -1,6 +1,13 @@
+type ParadoxEventListenerWeakMap = WeakMap<HTMLElement, Map<string, EventListener>>;
 // WeakMap to store event listeners for each element
-const eventListeners: WeakMap<HTMLElement, Map<string, EventListener>> = new WeakMap();
+const eventListeners: ParadoxEventListenerWeakMap = new WeakMap();
 
+/**
+ * Attaches event listeners to an HTML element.
+ * 
+ * @param element - The HTML element to attach the event listeners to.
+ * @param events - An object containing event names as keys and event listeners as values.
+ */
 function handleEvents(element: HTMLElement, events: { [key: string]: EventListener }): void {
   // Retrieve or create the event listeners Map for this particular element
   let elementEvents = eventListeners.get(element);
