325cc3e28ca8034580a737c9f3e891c45a29e05e
alexsc6955 <rincorpes@gmail.com>
2024-01-17 10:34:55 -0500

    Added module buildApp

---
+++ build/core/buildApp/index.d.ts
@@ -0,0 +1,4 @@
+export declare function addState(value: any): any[];
+export declare function onStateChange(proxyObj: any, callback: any): () => void;
+export declare function addEffect(fn: Function): void;
+export default function app(treeFunc: Function, targetNode: HTMLElement): void;
