cb9f4be18554e6ce9b68d5bdc715c423d464f917
alexsc6955 <rincorpes@gmail.com>
2024-01-24 14:03:56 -0500

    Remove createElement function and related files

--- build/core/buildElement/helpers/createElement.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Creates an HTML element with the specified element name.
- * If the element has been created before, it returns a clone of the cached element.
- * @param elementName - The name of the HTML element to create.
- * @returns The created HTML element.
- */
-declare function createElement(elementName: string): HTMLElement;
-export default createElement;
